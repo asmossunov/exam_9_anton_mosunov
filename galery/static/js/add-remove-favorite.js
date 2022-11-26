af2f1f8359c8adba86be0a1cbb0f8589ce3eb9a4
Mosunov Anton <antonmossunov@gmail.com>
2022-11-26 19:06:47 +0600

    Начало работы с JS для отравки запроса на добавление или удаление в избранное

--- galery/static/js/add-remove-favorite.js
+++ galery/static/js/add-remove-favorite.js
@@ -3,13 +3,18 @@ window.addEventListener('load', function() {
     let buttonRemove = $('#remove-from-favorite');
 
     buttonAdd.on('click', function(evt) {
-        buttonRemove.remove()
+        const pk = $('button[data-pk]').data('pk')
+        $('button[data-pk]').data('pk').remove()
+
+        let url = 'http://localhost:8000/photos/';
+
+            url : url + `${pk}/like/`,
+
 
         $.ajax({
             type: 'POST',
-            url : 'photos/to-favorite/',
+            url : url + `${pk}/to-favorite/`,
             data: {
-
                 csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
                 action:'post',
             },
