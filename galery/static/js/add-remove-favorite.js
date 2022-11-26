555495b9e2bc61304b54ca970019df0435a63e67
Mosunov Anton <antonmossunov@gmail.com>
2022-11-26 19:08:40 +0600

    Рефакторинг кода

--- galery/static/js/add-remove-favorite.js
+++ galery/static/js/add-remove-favorite.js
@@ -1,29 +1,29 @@
-window.addEventListener('load', function() {
-    let buttonAdd= $('#add-to-favorite');
-    let buttonRemove = $('#remove-from-favorite');
-
-    buttonAdd.on('click', function(evt) {
-        const pk = $('button[data-pk]').data('pk')
-        $('button[data-pk]').data('pk').remove()
-
-        let url = 'http://localhost:8000/photos/';
-
-            url : url + `${pk}/like/`,
-
-
-        $.ajax({
-            type: 'POST',
-            url : url + `${pk}/to-favorite/`,
-            data: {
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
-                action:'post',
-            },
-            success: function(data) {
-
-                alert('Опыт работы добавлен!')
-            }
-        })
-
-
-    });
-}
+// window.addEventListener('load', function() {
+//     let buttonAdd= $('#add-to-favorite');
+//     let buttonRemove = $('#remove-from-favorite');
+//
+//     buttonAdd.on('click', function(evt) {
+//         const pk = $('button[data-pk]').data('pk')
+//         $('button[data-pk]').data('pk').remove()
+//
+//         let url = 'http://localhost:8000/photos/';
+//
+//             url : url + `${pk}/like/`,
+//
+//
+//         $.ajax({
+//             type: 'POST',
+//             url : url + `${pk}/to-favorite/`,
+//             data: {
+//                 csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+//                 action:'post',
+//             },
+//             success: function(data) {
+//
+//                 alert('Опыт работы добавлен!')
+//             }
+//         })
+//
+//
+//     });
+// }
